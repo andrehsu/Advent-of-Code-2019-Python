5dd94421bb0c26ed807b18aea8d43d5094d21370
Andre Hsu <4470828+andrehsu@users.noreply.github.com>
2020-06-04 04:35:25 +0800

    Add test_case util function

--- adventofcode/utils.py
+++ adventofcode/utils.py
@@ -3,3 +3,7 @@ from typing import List
 
 def read_input(day: int) -> List[str]:
     return open(f'inputs/day{day}.txt').read().splitlines()
+
+
+def test_case(raw_str: str) -> List[str]:
+    return raw_str.strip('\n').splitlines()
