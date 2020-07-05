f72114f866ba4d8614c6aa565173a8a4a4cdc328
Andre Hsu <4470828+andrehsu@users.noreply.github.com>
2020-07-05 23:59:36 +0800

    Change type annotation to use Typing Introduce instruction pointer as a field

--- adventofcode/day7.py
+++ adventofcode/day7.py
@@ -1,12 +1,12 @@
 from itertools import permutations
-from typing import Optional
+from typing import Optional, Tuple
 
 from utils import read_input
 
 INPUT = read_input(7)[0]
 
 
-def parse_instruction(instruction: int) -> tuple[int, int, int, int]:
+def parse_instruction(instruction: int) -> Tuple[int, int, int, int]:
     return (instruction % 100 // 1,
             instruction % 1000 // 100,
             instruction % 10000 // 1000,
@@ -15,66 +15,66 @@ def parse_instruction(instruction: int) -> tuple[int, int, int, int]:
 
 class Amplifier:
     def __init__(self, code: str, phase_setting: int):
+        self.i = 0
         self.mem = list(map(int, code.split(',')))
         
         self.inputs = [phase_setting]
     
     def run(self, input_signal: int) -> Optional[int]:
         self.inputs.append(input_signal)
-        
+
         def param(num: int) -> int:
-            return self.mem[i + num]
+            return self.mem[self.i + num]
         
         def param_value(param_num: int) -> int:
             p = param(param_num)
             return p if modes[param_num - 1] == 1 else self.mem[p]
-        
-        i = 0
+
         while True:
-            opcode, *modes = parse_instruction(self.mem[i])
+            opcode, *modes = parse_instruction(self.mem[self.i])
             
             if opcode == 1:
                 self.mem[param(3)] = param_value(1) + param_value(2)
                 
                 assert modes[2] == 0
-                
-                i += 4
+
+                self.i += 4
             elif opcode == 2:
                 self.mem[param(3)] = param_value(1) * param_value(2)
                 
                 assert modes[2] == 0
-                
-                i += 4
+
+                self.i += 4
             elif opcode == 3:
                 self.mem[param(1)] = self.inputs.pop(0)
                 
                 assert modes[0] == 0
-                
-                i += 2
+
+                self.i += 2
             elif opcode == 4:
                 return param_value(1)
             elif opcode == 5:
                 if param_value(1) != 0:
-                    i = param_value(2)
+                    self.i = param_value(2)
                 else:
-                    i += 3
+                    self.i += 3
             elif opcode == 6:
                 if param_value(1) == 0:
-                    i = param_value(2)
+                    self.i = param_value(2)
                 else:
-                    i += 3
+                    self.i += 3
             elif opcode == 7:
                 if param_value(1) < param_value(2):
                     self.mem[param(3)] = 1
                 else:
                     self.mem[param(3)] = 0
-                i += 4
+                self.i += 4
             elif opcode == 8:
                 if param_value(1) == param_value(2):
                     self.mem[param(3)] = 1
                 else:
                     self.mem[param(3)] = 0
-                i += 4
+                self.i += 4
             elif opcode == 99:
                 return None
             else:
