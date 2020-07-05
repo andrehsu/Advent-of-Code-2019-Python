80d593a43c0fc5bfe5796450b23124433d8254ce
Andre Hsu <4470828+andrehsu@users.noreply.github.com>
2020-07-05 23:35:02 +0800

    Change day 7 part 1 to use Amplifier class

--- adventofcode/day7.py
+++ adventofcode/day7.py
@@ -1,4 +1,5 @@
 from itertools import permutations
+from typing import Optional
 
 from utils import read_input
 
@@ -12,69 +13,72 @@ def parse_instruction(instruction: int) -> tuple[int, int, int, int]:
             instruction % 100000 // 10000)
 
 
-def program(code, phase_setting: int, input_signal: int) -> int:
-    inputs = [phase_setting, input_signal]
-    
-    mem = list(map(int, code.split(',')))
-    
-    def param(num: int) -> int:
-        return mem[i + num]
-    
-    def param_value(param_num: int) -> int:
-        p = param(param_num)
-        return p if modes[param_num - 1] == 1 else mem[p]
-    
-    i = 0
-    while True:
-        opcode, *modes = parse_instruction(mem[i])
+class Amplifier:
+    def __init__(self, code: str, phase_setting: int):
+        self.mem = list(map(int, code.split(',')))
         
-        if opcode == 1:
-            mem[param(3)] = param_value(1) + param_value(2)
+        self.inputs = [phase_setting]
+    
+    def run(self, input_signal: int) -> Optional[int]:
+        self.inputs.append(input_signal)
+        
+        def param(num: int) -> int:
+            return self.mem[i + num]
+        
+        def param_value(param_num: int) -> int:
+            p = param(param_num)
+            return p if modes[param_num - 1] == 1 else self.mem[p]
+        
+        i = 0
+        while True:
+            opcode, *modes = parse_instruction(self.mem[i])
             
-            assert modes[2] == 0
-            
-            i += 4
-        elif opcode == 2:
-            mem[param(3)] = param_value(1) * param_value(2)
-            
-            assert modes[2] == 0
-            
-            i += 4
-        elif opcode == 3:
-            mem[param(1)] = inputs.pop(0)
-            
-            assert modes[0] == 0
-            
-            i += 2
-        elif opcode == 4:
-            return param_value(1)
-        elif opcode == 5:
-            if param_value(1) != 0:
-                i = param_value(2)
+            if opcode == 1:
+                self.mem[param(3)] = param_value(1) + param_value(2)
+                
+                assert modes[2] == 0
+                
+                i += 4
+            elif opcode == 2:
+                self.mem[param(3)] = param_value(1) * param_value(2)
+                
+                assert modes[2] == 0
+                
+                i += 4
+            elif opcode == 3:
+                self.mem[param(1)] = self.inputs.pop(0)
+                
+                assert modes[0] == 0
+                
+                i += 2
+            elif opcode == 4:
+                return param_value(1)
+            elif opcode == 5:
+                if param_value(1) != 0:
+                    i = param_value(2)
+                else:
+                    i += 3
+            elif opcode == 6:
+                if param_value(1) == 0:
+                    i = param_value(2)
+                else:
+                    i += 3
+            elif opcode == 7:
+                if param_value(1) < param_value(2):
+                    self.mem[param(3)] = 1
+                else:
+                    self.mem[param(3)] = 0
+                i += 4
+            elif opcode == 8:
+                if param_value(1) == param_value(2):
+                    self.mem[param(3)] = 1
+                else:
+                    self.mem[param(3)] = 0
+                i += 4
+            elif opcode == 99:
+                return None
             else:
-                i += 3
-        elif opcode == 6:
-            if param_value(1) == 0:
-                i = param_value(2)
-            else:
-                i += 3
-        elif opcode == 7:
-            if param_value(1) < param_value(2):
-                mem[param(3)] = 1
-            else:
-                mem[param(3)] = 0
-            i += 4
-        elif opcode == 8:
-            if param_value(1) == param_value(2):
-                mem[param(3)] = 1
-            else:
-                mem[param(3)] = 0
-            i += 4
-        elif opcode == 99:
-            print()
-            break
-        else:
-            raise RuntimeError(f"Unexpected opcode: {opcode}")
+                raise RuntimeError(f"Unexpected opcode: {opcode}")
 
 
 def day5(input_: str):
@@ -84,11 +88,11 @@ def day5(input_: str):
 def part1(input_):
     signals = []
     for a, b, c, d, e in permutations(range(5)):
-        a_output = program(input_, a, 0)
-        b_output = program(input_, b, a_output)
-        c_output = program(input_, c, b_output)
-        d_output = program(input_, d, c_output)
-        e_output = program(input_, e, d_output)
+        a_output = Amplifier(input_, a).run(0)
+        b_output = Amplifier(input_, b).run(a_output)
+        c_output = Amplifier(input_, c).run(b_output)
+        d_output = Amplifier(input_, d).run(c_output)
+        e_output = Amplifier(input_, e).run(d_output)
         signals.append(e_output)
     print(max(signals))
 
