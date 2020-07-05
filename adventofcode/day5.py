a4c4b893c77bd740bf3fe7d6ef360ccddf10e975
Andre Hsu <4470828+andrehsu@users.noreply.github.com>
2020-07-05 23:14:08 +0800

    Finish day 5 part 2

--- adventofcode/day5.py
+++ adventofcode/day5.py
@@ -1,21 +1,19 @@
-from typing import List, Tuple
-
 from utils import read_input
 
 INPUT = read_input(5)[0]
 
-Memory = List[int]
+Memory = list[int]
 
 
-def parse_instruction(instruction: int) -> Tuple[int, int, int, int]:
+def parse_instruction(instruction: int) -> tuple[int, int, int, int]:
     return (instruction % 100 // 1,
             instruction % 1000 // 100,
             instruction % 10000 // 1000,
             instruction % 100000 // 10000)
 
 
-def program(mem: Memory, id_: int) -> None:
-    mem = mem.copy()
+def program(input_, id_: int) -> None:
+    mem = list(map(int, input_.split(',')))
     
     def param(num: int) -> int:
         return mem[i + num]
@@ -24,18 +22,20 @@ def program(mem: Memory, id_: int) -> None:
     while True:
         opcode, *modes = parse_instruction(mem[i])
         
-        def read_mem_at_param(param_num: int) -> int:
+        def param_value(param_num: int) -> int:
             param_value = param(param_num)
             return param_value if modes[param_num - 1] == 1 else mem[param_value]
         
+        before_instruction = mem[i]
+        
         if opcode == 1:
-            mem[param(3)] = read_mem_at_param(1) + read_mem_at_param(2)
+            mem[param(3)] = param_value(1) + param_value(2)
             
             assert modes[2] == 0
             
             i += 4
         elif opcode == 2:
-            mem[param(3)] = read_mem_at_param(1) * read_mem_at_param(2)
+            mem[param(3)] = param_value(1) * param_value(2)
             
             assert modes[2] == 0
             
@@ -47,22 +47,49 @@ def program(mem: Memory, id_: int) -> None:
             
             i += 2
         elif opcode == 4:
-            print(read_mem_at_param(1), end=' ')
+            print(param_value(1), end=' ')
             
             i += 2
+        elif opcode == 5:
+            if param_value(1) != 0:
+                i = param_value(2)
+            else:
+                i += 3
+        elif opcode == 6:
+            if param_value(1) == 0:
+                i = param_value(2)
+            else:
+                i += 3
+        elif opcode == 7:
+            if param_value(1) < param_value(2):
+                mem[param(3)] = 1
+            else:
+                mem[param(3)] = 0
+            i += 4
+        elif opcode == 8:
+            if param_value(1) == param_value(2):
+                mem[param(3)] = 1
+            else:
+                mem[param(3)] = 0
+            i += 4
         elif opcode == 99:
+            print()
             break
         else:
             raise RuntimeError(f"Unexpected opcode: {opcode}")
 
 
 def day5(input_: str):
-    mem = list(map(int, input_.split(',')))
-    part1(mem)
+    part1(input_)
+    part2(input_)
 
 
-def part1(mem: Memory):
-    program(mem, 1)
+def part1(input_):
+    program(input_, 1)
+
+
+def part2(input_):
+    program(input_, 5)
 
 
 day5(INPUT)
