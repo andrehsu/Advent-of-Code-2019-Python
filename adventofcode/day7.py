dde28bca21a0fd2a2d28e4550e5677d866069445
Andre Hsu <4470828+andrehsu@users.noreply.github.com>
2020-07-06 00:44:28 +0800

    Finish day 7 part 2

--- adventofcode/day7.py
+++ adventofcode/day7.py
@@ -47,12 +47,14 @@ class Amplifier:
                 self.i += 4
             elif opcode == 3:
                 self.mem[param(1)] = self.inputs.pop(0)
-                
+    
                 assert modes[0] == 0
-
+    
                 self.i += 2
             elif opcode == 4:
-                return param_value(1)
+                ret_val = param_value(1)
+                self.i += 2
+                return ret_val
             elif opcode == 5:
                 if param_value(1) != 0:
                     self.i = param_value(2)
@@ -81,8 +83,9 @@ class Amplifier:
                 raise RuntimeError(f"Unexpected opcode: {opcode}")
 
 
-def day5(input_: str):
+def day7(input_: str):
     part1(input_)
+    part2(input_)
 
 
 def part1(input_):
@@ -97,4 +100,43 @@ def part1(input_):
     print(max(signals))
 
 
-day5(INPUT)
+def part2(input_):
+    signals = []
+    
+    for a, b, c, d, e in permutations(range(5, 10)):
+        amplifiers = {
+            'a': Amplifier(input_, a),
+            'b': Amplifier(input_, b),
+            'c': Amplifier(input_, c),
+            'd': Amplifier(input_, d),
+            'e': Amplifier(input_, e),
+        }
+        
+        last_output = 0
+        
+        last_loop = False
+        while not last_loop:
+            for key, amplifier in amplifiers.items():
+                output = amplifier.run(last_output)
+                if output is None:
+                    last_loop = True
+                else:
+                    last_output = output
+        
+        signals.append(last_output)
+    
+    print(max(signals))
+
+
+# # day5(INPUT)
+# test = '3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0'
+# part1(test)
+# test= '3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0'
+# part1(test)
+# test = '3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5'
+# part2(test)
+# test = '3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,' \
+#         '1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10'
+# part2(test)
+
+day7(INPUT)
