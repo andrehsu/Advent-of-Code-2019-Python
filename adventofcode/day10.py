2fc4e8b6e7a2b8586d4048776638afdacdcb1d97
Andre Hsu <4470828+andrehsu@users.noreply.github.com>
2020-09-17 22:03:05 +0800

    Simplify optimization of part 1 Add part 2

--- adventofcode/day10.py
+++ adventofcode/day10.py
@@ -1,63 +1,119 @@
+import math
+from collections import Counter
+from functools import lru_cache
+from operator import itemgetter
 from typing import List, Tuple
 
 from utils import read_input, test_case
 
 INPUT = read_input(10)
 
-Map = List[List[bool]]
 Pos = Tuple[int, int]
 
 
-def parse_map(inp: List[str]) -> Map:
-    return [[i == '#' for i in row] for row in inp]
+def parse_asteroids(inp: List[str]) -> List[Pos]:
+    asteroids = [(x, y) for y, row in enumerate(inp) for x, v in enumerate(row) if v == '#']
+    
+    return asteroids
 
 
-def print_map(space_map: Map) -> None:
-    for row in space_map:
-        for i in row:
-            if i:
-                print('#', end='')
-            else:
-                print('.', end='')
-        print()
-
-
-def is_visible(asteroids: List[Pos], a: Pos, b: Pos) -> bool:
+@lru_cache(maxsize=None)
+def angle_between(a: Pos, b: Pos) -> float:
     ax, ay = a
     bx, by = b
-    for c in asteroids:
-        cx, cy = c
-        if ((cy - ay) * (bx - ax) - (cx - ax) * (by - ay) == 0 and
-                (ax <= cx <= bx or ax >= cx >= bx) and
-                (ay <= cy <= by or ay >= cy >= by) and
-                len({a, b, c}) == 3):
-            return False
-    return True
+    
+    angle = math.atan2(-by - -ay, bx - ax) * 180 / math.pi
+    
+    angle -= 90
+    
+    if angle < 0:
+        angle += 360
+    
+    angle = 360 - angle
+    if angle == 360:
+        angle = 0
+    
+    return angle
+
+
+def best_spot(asteroids: List[Pos]) -> Tuple[Pos, int]:
+    def is_visible(a: Pos, b: Pos) -> bool:
+        if b > a:
+            return _is_visible(b, a)
+        else:
+            return _is_visible(a, b)
+    
+    # noinspection DuplicatedCode
+    @lru_cache(maxsize=None)
+    def _is_visible(a: Pos, b: Pos) -> bool:
+        ax, ay = a
+        bx, by = b
+        for c in asteroids:
+            cx, cy = c
+            if ((cy - ay) * (bx - ax) - (cx - ax) * (by - ay) == 0 and
+                    (ax <= cx <= bx or ax >= cx >= bx) and
+                    (ay <= cy <= by or ay >= cy >= by) and
+                    len({a, b, c}) == 3):
+                return False
+        return True
+    
+    counts = Counter()
+    
+    for a in asteroids:
+        for b in asteroids:
+            if is_visible(a, b):
+                counts[a] += 1
+    
+    pos, count = counts.most_common(1)[0]
+    
+    return pos, count - 1
 
 
 def part1(inp: List[str]) -> None:
-    space_map = parse_map(inp)
+    asteroids = parse_asteroids(inp)
     
-    asteroids = [(r, c) for r, row in enumerate(space_map) for c, v in enumerate(row) if v]
+    print(best_spot(asteroids)[1])
+
+
+def part2(inp: List[str]) -> None:
+    asteroids = parse_asteroids(inp)
     
-    visible = {}
+    station = best_spot(asteroids)[0]
     
-    for a in asteroids:
-        for b in asteroids:
-            if a != b and (a, b) not in visible and (b, a) not in visible:
-                visibility = is_visible(asteroids, a, b)
-                visible[(a, b)] = visibility
-                visible[(b, a)] = visibility
+    asteroids.remove(station)
     
-    counts = {}
+    # noinspection DuplicatedCode
+    def is_visible(a: Pos, b: Pos) -> bool:
+        ax, ay = a
+        bx, by = b
+        for c in asteroids:
+            cx, cy = c
+            if ((cy - ay) * (bx - ax) - (cx - ax) * (by - ay) == 0 and
+                    (ax <= cx <= bx or ax >= cx >= bx) and
+                    (ay <= cy <= by or ay >= cy >= by) and
+                    len({a, b, c}) == 3):
+                return False
+        return True
     
-    for a in asteroids:
-        counts[a] = 0
-        for b in asteroids:
-            if a != b and visible[(a, b)]:
-                counts[a] += 1
+    last = None
+    deg = 0
+    for i in range(200):
+        angles = [(angle_between(station, a), a) for a in asteroids if is_visible(station, a)]
+        angles = filter(lambda x: x[0] >= deg, angles)
+        
+        if not angles:
+            deg = 0
+            continue
+        
+        angle, ast = min(angles, key=itemgetter(0))
+        last = ast
+        deg = angle + 0.0001
+        print(f'{i + 1}th asteroid: {last} at deg {angle:.2f}')
+        asteroids.remove(ast)
     
-    print(max(counts.values()))
+    print(last)
+    
+    print(last[0] * 100 + last[1])
 
 
 test0 = test_case('''
@@ -92,6 +148,5 @@ test1 = test_case('''
 ''')
 
 if __name__ == '__main__':
-    # part1(test0)
-    # part1(test1)
-    part1(INPUT)
+    # part1(INPUT)
+    part2(INPUT)
