30209ceda018861164a52150a2289e121a249ed3
Andre Hsu <4470828+andrehsu@users.noreply.github.com>
2020-09-18 14:30:01 +0800

    Finish day 12 part 2

--- adventofcode/day12.py
+++ adventofcode/day12.py
@@ -1,3 +1,4 @@
+import math
 import re
 from dataclasses import dataclass
 from itertools import combinations
@@ -16,19 +17,43 @@ class Moon:
     dx: int = 0
     dy: int = 0
     dz: int = 0
-    
+
     @staticmethod
     def from_str(s: str) -> 'Moon':
         x, y, z = map(int, re.findall(r'-?\d+', s))
         return Moon(x, y, z)
-    
+
     def move(self):
         self.x += self.dx
         self.y += self.dy
         self.z += self.dz
-    
+
+    def apply_gravity(self, other):
+        if self.x > other.x:
+            self.dx -= 1
+            other.dx += 1
+        elif other.x > self.x:
+            other.dx -= 1
+            self.dx += 1
+        if self.y > other.y:
+            self.dy -= 1
+            other.dy += 1
+        elif other.y > self.y:
+            other.dy -= 1
+            self.dy += 1
+        if self.z > other.z:
+            self.dz -= 1
+            other.dz += 1
+        elif other.z > self.z:
+            other.dz -= 1
+            self.dz += 1
+
     def __str__(self):
         return f'pos=<x={self.x:>2}, y={self.y:>2}, z={self.z:>2}>, vel<x={self.dx:>2}, y={self.dy:>2}, z={self.dz:>2}>'
+
+
+def lcm(a, b):
+    return a * b // math.gcd(a, b)
 
 
 def parse_input(inp: List[str]) -> List[Moon]:
@@ -50,44 +75,60 @@ def total_energy(moons: List[Moon]) -> int:
     return te
 
 
-def simulate(moons: List[Moon], steps: int) -> List[Moon]:
+def part1(inp: List[str], steps: int = 1000) -> None:
+    moons = parse_input(inp)
     for i in range(steps):
-        # print(f'After {i} steps:')
-        # for moon in moons:
-        #     print(moon)
-        # print()
         for a, b in combinations(moons, 2):
-            if a.x > b.x:
-                a.dx -= 1
-                b.dx += 1
-            elif b.x > a.x:
-                b.dx -= 1
-                a.dx += 1
-            if a.y > b.y:
-                a.dy -= 1
-                b.dy += 1
-            elif b.y > a.y:
-                b.dy -= 1
-                a.dy += 1
-            if a.z > b.z:
-                a.dz -= 1
-                b.dz += 1
-            elif b.z > a.z:
-                b.dz -= 1
-                a.dz += 1
+            a.apply_gravity(b)
         
         for a in moons:
             a.move()
     
-    return moons
+    print(total_energy(moons))
 
 
-def part1(inp: List[str], steps=1000) -> None:
+def part2(inp: List[str]) -> None:
     moons = parse_input(inp)
     
-    moons = simulate(moons, steps)
+    xs_loop = ys_loop = zs_loop = -1
     
-    print(total_energy(moons))
+    x_seen = set()
+    y_seen = set()
+    z_seen = set()
+    
+    step = 0
+    while True:
+        xs = tuple((x.x, x.dx) for x in moons)
+        ys = tuple((x.y, x.dy) for x in moons)
+        zs = tuple((x.z, x.dz) for x in moons)
+        
+        if xs_loop != -1 and ys_loop != -1 and zs_loop != -1:
+            break
+        
+        if xs_loop == -1 and xs in x_seen:
+            xs_loop = step
+        else:
+            x_seen.add(xs)
+        
+        if ys_loop == -1 and ys in y_seen:
+            ys_loop = step
+        else:
+            y_seen.add(ys)
+        
+        if zs_loop == -1 and zs in z_seen:
+            zs_loop = step
+        else:
+            z_seen.add(zs)
+        
+        for a, b in combinations(moons, 2):
+            a.apply_gravity(b)
+        
+        for a in moons:
+            a.move()
+        
+        step += 1
+    
+    print(lcm(lcm(xs_loop, ys_loop), zs_loop))
 
 
 test0 = test_case("""
@@ -105,6 +146,9 @@ test1 = test_case("""
 """)
 
 if __name__ == '__main__':
-    part1(test0, 10)
-    part1(test1, 100)
+    # part1(test0, 10)
+    # part1(test1, 100)
     part1(INPUT)
+    part2(test0)
+    # part2(test1)
+    part2(INPUT)
